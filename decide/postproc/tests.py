525ab651b210f5b40068d775e647226a7f8711a0
Daniel <dansanbal@alum.us.es>
2020-12-24 17:20:48 +0100

    postprocesado - #42 - Votación por preferencia - #40 - Recuento Borda
    postprocesado - #42 - Implementado soporte para votación por preferencia - #40 - Procesado de votos según el recuento Borda

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -42,3 +42,27 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
+    def test_borda(self):
+        data = {
+            'type': 'BORDA',
+            'options': [
+                {'option': 'Option 1', 'number': 1, 'votes': [7,2,4,2]},
+                {'option': 'Option 2', 'number': 2, 'votes': [2,8,2,3]},
+                {'option': 'Option 3', 'number': 3, 'votes': [4,4,4,3]},
+                {'option': 'Option 4', 'number': 4, 'votes': [2,1,5,7]},
+            ]
+        }
+
+        expected_result = [
+            {'option': 'Option 1', 'number': 1, 'votes': [7,2,4,2], 'postproc': 44},
+            {'option': 'Option 2', 'number': 2, 'votes': [2,8,2,3], 'postproc': 39},
+            {'option': 'Option 3', 'number': 3, 'votes': [4,4,4,3], 'postproc': 39},
+            {'option': 'Option 4', 'number': 4, 'votes': [2,1,5,7], 'postproc': 28},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
