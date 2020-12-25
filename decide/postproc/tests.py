5c861428b8d2f42283350d3d89a3c683325c7063
Jorge <Raposo>
2020-12-25 05:16:22 +0000

    postprocesado - #43 - Implementada paridad

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -42,3 +42,31 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+    
+    def test_equality(self):
+        data = {
+            'type': 'EQUALITY',
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes_men': 2, 'votes_women': 3 },
+                { 'option': 'Option 2', 'number': 2, 'votes_men': 0, 'votes_women': 4 },
+                { 'option': 'Option 3', 'number': 3, 'votes_men': 3, 'votes_women': 1 },
+                { 'option': 'Option 4', 'number': 4, 'votes_men': 1, 'votes_women': 0 },
+                { 'option': 'Option 5', 'number': 5, 'votes_men': 1, 'votes_women': 3 },
+                { 'option': 'Option 6', 'number': 6, 'votes_men': 1, 'votes_women': 1 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Option 1', 'number': 1, 'votes_men': 2, 'votes_women': 3, 'postproc': 4 },
+            { 'option': 'Option 3', 'number': 3, 'votes_men': 3, 'votes_women': 1, 'postproc': 4 },
+            { 'option': 'Option 2', 'number': 2, 'votes_men': 0, 'votes_women': 4, 'postproc': 3 },
+            { 'option': 'Option 5', 'number': 5, 'votes_men': 1, 'votes_women': 3, 'postproc': 3 },
+            { 'option': 'Option 6', 'number': 6, 'votes_men': 1, 'votes_women': 1, 'postproc': 2 },
+            { 'option': 'Option 4', 'number': 4, 'votes_men': 1, 'votes_women': 0, 'postproc': 1 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -11,7 +11,39 @@ class PostProcView(APIView):
             out.append({
                 **opt,
                 'postproc': opt['votes'],
-            });
+            })
+
+        out.sort(key=lambda x: -x['postproc'])
+        return Response(out)
+
+    def equality(self, options):
+        out = []
+        n_women = 0
+        n_men = 0
+        rel = 0.
+        is_men_greater = False
+
+        for opt in options:
+            n_women += opt['votes_women']
+            n_men += opt['votes_men']
+
+        if n_women > n_men:
+            rel = n_men/n_women
+        else:
+            rel = n_women/n_men
+            is_men_greater = True
+
+        for opt in options:
+            votes = 0
+            if is_men_greater:
+                votes = opt['votes_women'] + opt['votes_men']*rel
+            else:
+                votes = opt['votes_men'] + opt['votes_women']*rel
+            
+            out.append({
+                **opt,
+                'postproc': round(votes),
+            })
 
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
@@ -27,6 +59,16 @@ class PostProcView(APIView):
              ...extraparams
             }
            ]
+        
+        * type: EQUALITY
+        * options: [
+            {
+             option: str,
+             number: int,
+             votes_men: int,
+             votes_women: int,
+            }
+           ]
         """
 
         t = request.data.get('type', 'IDENTITY')
@@ -34,5 +76,7 @@ class PostProcView(APIView):
 
         if t == 'IDENTITY':
             return self.identity(opts)
+        if t == 'EQUALITY':
+            return self.equality(opts)
 
         return Response({})
