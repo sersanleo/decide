67047b206a5d05f7ba40b53f18a3514636c1b873
marmolpen3 <marmolpen3@alum.us.es>
2020-12-29 18:04:04 +0100

    cabina-007-Adaptar la interfaz gráfica para personas daltónicas
    Se añade un botón para cambiar el estilo a deuteranopia.

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BoothView
+from .views import BoothView, style_change
 
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
+    path('9cff8fe0-b7e3-48cb-970f-13ae47f89426/', style_change, name = 'deuteranopia'),
 ]
--- decide/booth/views.py
+++ decide/booth/views.py
@@ -2,12 +2,13 @@ import json
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
-
+from django.shortcuts import render, get_object_or_404
 from base import mods
+from voting.models import Voting
 
 
 # TODO: check permissions and census
-class BoothView(TemplateView):
+class BoothView(TemplateView): 
     template_name = 'booth/booth.html'
 
     def get_context_data(self, **kwargs):
@@ -23,9 +24,33 @@ class BoothView(TemplateView):
                 r[0]['pub_key'][k] = str(v)
 
             context['voting'] = json.dumps(r[0])
+            context['votacion_id'] = vid
+
         except:
             raise Http404
 
         context['KEYBITS'] = settings.KEYBITS
 
         return context
+
+def style_change(request):
+    if request.method == 'POST': 
+        vid = request.POST['votacion'] 
+
+        try:
+            r = mods.get('voting', params={'id': vid})
+
+            # Casting numbers to string to manage in javascript with BigInt
+            # and avoid problems with js and big number conversion
+            for k, v in r[0]['pub_key'].items():
+                r[0]['pub_key'][k] = str(v)
+
+            return render(request, 'booth/booth.html', {'d_p':True, 'KEYBITS':settings.KEYBITS, 'voting':json.dumps(r[0])})
+
+        except:
+            raise Http404
+
+        return render(request, 'booth/booth.html', {'d_p':False, 'KEYBITS':settings.KEYBITS})
+
+    else:
+        return render(request, 'booth/booth.html', {'error':"Debes pasar primero por login"})
