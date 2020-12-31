80964d9eae670865257ff9f6d8aed61da4fdf961
marmolpen3 <marmolpen3@alum.us.es>
2020-12-31 19:10:55 +0100

    010 - Dashboard sobre votaciones de usuario
    Refactorización de cabina de votación.

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from booth import views as booth_views
 
-
 urlpatterns = [
     path('', booth_views.LoginView.as_view(), name="login"),
-    path('dashboard/<int:user_id>/', booth_views.DashboardView.as_view(), name="dashboard"),
-    path('<int:voting_id>/<int:user_id>/', booth_views.BoothView.as_view(), name="votacion"),
+    path('logout/', booth_views.LogoutView.as_view(), name="logout"),
+    path('dashboard/', booth_views.dashboard_view, name="dashboard"),
+    path('<int:voting_id>/', booth_views.BoothView.as_view, name="votacion")
 ]
--- decide/booth/views.py
+++ decide/booth/views.py
@@ -12,35 +12,78 @@ from django.contrib.auth.models import User
 class LoginView(TemplateView):
     template_name = 'booth/login.html'
 
-class DashboardView(TemplateView):
-    template_name = 'booth/dashboard.html'
-
+class LogoutView(TemplateView):
+    template_name = 'booth/login.html'
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user_id = kwargs.get('user_id', 0)
-        context['user_id'] = user_id
-
-        vot_dis = []
-        no_censo, no_vot_dis = False, False
-
-        census_by_user = Census.objects.filter(voter_id=user_id)
-        if census_by_user.count() == 0 :
-            context['no_censo'] = True
-        else:
-            for c in census_by_user:
-                vid = c.voting_id
-                try:
-                    votacion = Voting.objects.filter(end_date__isnull=True).exclude(start_date__isnull=True).get(id=vid)
-                    if Vote.objects.filter(voting_id=vid, voter_id=user_id).count()==0:
-                        vot_dis.append(votacion)
-                except Exception:
-                    error= 'Esta votación ha sido borrada'
-        context['vot_dis'] = vot_dis
-
-        if len(vot_dis) == 0:
-            context['no_vot_dis'] = True
-
+        token = self.request.session.get('user_token')
+        if token: 
+            mods.post('authentication', entry_point='/logout/', json={'token':token})
+            del self.request.session['user_token']
+            del self.request.session['voter_id']
+    
         return context
+
+def autenticacion(request, username, password):
+    token= mods.post('authentication', entry_point='/login/', json={'username':username, 'password':password})
+    request.session['user_token']=token
+    voter = mods.post('authentication', entry_point='/getuser/', json=token)
+    voter_id = voter.get('id', None)
+    request.session['voter_id'] = voter_id
+    if voter_id == None:
+        return False, voter_id
+    return True, voter_id
+
+def dashboard_details(voter_id):
+    context={}
+    vot_dis=[]
+    context['no_censo'], context['no_vot_dis'] = False, False
+
+    census_by_user = Census.objects.filter(voter_id=voter_id)
+    if census_by_user.count() == 0 :
+        context['no_censo'] = True
+    else:
+        for c in census_by_user:
+            vid = c.voting_id
+            try:
+                votacion = Voting.objects.filter(end_date__isnull=True).exclude(start_date__isnull=True).get(id=vid)
+                if Vote.objects.filter(voting_id=vid, voter_id=voter_id).count()==0:
+                    vot_dis.append(votacion)
+            except Exception:
+                error= 'Esta votación ha sido borrada'
+    context['vot_dis'] = vot_dis
+    print(vot_dis)
+    if len(vot_dis) == 0:
+        context['no_vot_dis'] = True
+    
+    return context
+
+def dashboard_view(request):
+    
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+        # Autenticacion
+        voter, voter_id = autenticacion(request, username, password)
+        if not voter:
+            return render(request, 'booth/login.html', {'no_user':True})
+        else:
+            print(voter_id)
+            context = dashboard_details(voter_id)
+            return render(request, 'booth/dashboard.html', context)
+    else:
+
+        token = request.session.get('user_token', None)
+        if token == None:
+            return render(request, 'booth/login.html')
+        else:
+            voter_id = request.session.get('voter_id', None)
+            print(voter_id)
+            context = dashboard_details(voter_id)
+            return render(request, 'booth/dashboard.html', context)
+
 
 class BoothView(TemplateView):
     template_name = 'booth/booth.html'
@@ -48,7 +91,6 @@ class BoothView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
-        user_id = kwargs.get('user_id', 0)
         
         try:
             r = mods.get('voting', params={'id': vid})
