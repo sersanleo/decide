b2b5e73caff357b20276d60c41244ea907c2ba20
jorandmol <jorandmol@alum.us.es>
2020-12-31 15:39:47 +0100

    012 - Adaptar la interfaz gráfica para personas daltónicas mediante cambios de estilo dinámicos
    He cambiado la forma en la que se implementa dicho incremento funcional, el cual se realiza mediante cambios dinámicos de clase usando vue.js. Se han hecho algunas modificaciones en los estilos.
    Falta por comprobar que un asistente de texto pueda leer la pantalla correctamente.

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import BoothView, chage_style
+from .views import BoothView
 
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
-    path('reload/', chage_style, name = 'change_style'),
 ]
