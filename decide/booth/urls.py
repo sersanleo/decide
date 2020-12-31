913783f2da37016f8194ec40bb62355a32aae2a7
marmolpen3 <marmolpen3@alum.us.es>
2020-12-31 10:32:57 +0100

    cabina-010-Dashboard sobre votaciones de usuario
    Cambios en el dashboard

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -4,6 +4,6 @@ from booth import views as booth_views
 
 urlpatterns = [
     path('', booth_views.LoginView.as_view(), name="login"),
-    path('dashboard/', booth_views.dashboardView, name="dashboard"),
-    path('<int:voting_id>/', booth_views.BoothView.as_view(), name="votacion"),
+    path('dashboard/<int:user_id>/', booth_views.DashboardView.as_view(), name="dashboard"),
+    path('<int:voting_id>/<int:user_id>/', booth_views.BoothView.as_view(), name="votacion"),
 ]
--- decide/booth/views.py
+++ decide/booth/views.py
@@ -12,41 +12,35 @@ from django.contrib.auth.models import User
 class LoginView(TemplateView):
     template_name = 'booth/login.html'
 
-def dashboardView(request):
-    if request.method == 'POST':
-        vot_dis, vot_pen = [], []
-        no_censo, no_vot_dis, no_vot_pen = False, False, False
+class DashboardView(TemplateView):
+    template_name = 'booth/dashboard.html'
 
-        user_id = request.POST['user']
-        token = request.POST['token']
-        voter = mods.post('authentication', entry_point='/getuser/', json={'token': token})
-        voter_id = voter.get('id', None)
-        
-        if(int(user_id) == voter_id):
-            census_by_user = Census.objects.filter(voter_id=user_id)
-            if census_by_user.count() == 0 :
-                no_censo = True
-            else:
-                for c in census_by_user:
-                    vid = c.voting_id
-                    try:
-                        votacion = Voting.objects.filter(end_date__isnull=True).exclude(start_date__isnull=True).get(id=vid)
-                        if Vote.objects.filter(voting_id=vid, voter_id=user_id).count()==0:
-                            vot_dis.append(votacion)
-                        else:
-                            vot_pen.append(votacion)
-                    except Exception:
-                        error= 'Esta votación ha sido borrada'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_id = kwargs.get('user_id', 0)
+        context['user_id'] = user_id
+
+        vot_dis = []
+        no_censo, no_vot_dis = False, False
+
+        census_by_user = Census.objects.filter(voter_id=user_id)
+        if census_by_user.count() == 0 :
+            context['no_censo'] = True
+        else:
+            for c in census_by_user:
+                vid = c.voting_id
+                try:
+                    votacion = Voting.objects.filter(end_date__isnull=True).exclude(start_date__isnull=True).get(id=vid)
+                    if Vote.objects.filter(voting_id=vid, voter_id=user_id).count()==0:
+                        vot_dis.append(votacion)
+                except Exception:
+                    error= 'Esta votación ha sido borrada'
+        context['vot_dis'] = vot_dis
+
         if len(vot_dis) == 0:
-            no_vot_dis = True
-        if len(vot_pen) == 0:
-            no_vot_pen = True
+            context['no_vot_dis'] = True
 
-        return render(request, 'booth/dashboard.html', {'vot_dis':vot_dis, 'vot_pen': vot_pen, 
-        'no_censo':no_censo, 'no_vot_dis':no_vot_dis, 'no_vot_pen':no_vot_pen})
-        
-    else:
-        return render(request, 'booth/login.html')
+        return context
 
 class BoothView(TemplateView):
     template_name = 'booth/booth.html'
@@ -55,7 +49,7 @@ class BoothView(TemplateView):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
         user_id = kwargs.get('user_id', 0)
-        print(user_id)
+        
         try:
             r = mods.get('voting', params={'id': vid})
 
@@ -66,8 +60,10 @@ class BoothView(TemplateView):
 
             context['voting'] = json.dumps(r[0])
 
+            if Vote.objects.filter(voting_id=vid, voter_id=user_id).count()!=0:
+                context['voted'] = True 
         except:
-            raise Http404
+            raise Http404 
 
         context['KEYBITS'] = settings.KEYBITS
 
