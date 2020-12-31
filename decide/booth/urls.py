4ff4ba6080668d235110a7fea75c95ae2f3dbea1
marmolpen3 <marmolpen3@alum.us.es>
2020-12-31 21:01:35 +0100

    cabina-013-Integracion de tareas
    Modificación en la visualización de las votaciones

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', booth_views.LoginView.as_view(), name="login"),
     path('logout/', booth_views.LogoutView.as_view(), name="logout"),
     path('dashboard/', booth_views.dashboard_view, name="dashboard"),
-    path('<int:voting_id>/', booth_views.BoothView.as_view, name="votacion")
+    path('<int:voting_id>/', booth_views.BoothView.as_view(), name="votacion")
 ]
