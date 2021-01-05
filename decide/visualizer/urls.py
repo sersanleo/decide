e20e64cd5d495773490c769c5849026311c6295c
Alberto <murillojaenesalberto@gmail.com>
2021-01-05 18:46:36 +0100

    015 - Estadisticas globales de las votaciones
    Añadida la vista con estadisticas gobales

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import VisualizerView, get_list_votings
+from .views import VisualizerView, get_global_view
+
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
-    path('', get_list_votings),
+    path('global', get_global_view),
 ]
--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -1,14 +1,13 @@
 import json
-
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.conf import settings
 from django.http import Http404
-
+from census.models import Census
+from store.models import Vote
+from voting.models import Voting
 from base import mods
 
-from voting.models import Voting
-from store.models import Vote
-from census.models import Census
 
 
 class VisualizerView(TemplateView):
@@ -31,38 +30,68 @@ class VisualizerView(TemplateView):
             context['voting'] = json.dumps(r[0])
             context['stats'] = json.dumps(stat)
         except:
+            
             raise Http404
-
+        
         return context
 
-def get_list_votings(request):
-    filter = request.GET.get('filter')
-    busqueda = request.GET.get('nombre')
-    list = None
+def get_global_view(request):
+
+    vt = 0
+    ct = 0
+    nvs = 0
+
     try:
-        if filter == 'F':
-            list = Voting.objects.filter(start_date__isnull=False, end_date__isnull=False).all()
-        elif filter == 'A':
-            list = Voting.objects.filter(start_date__isnull=False, end_date__isnull=True).all()
-        elif filter == 'S':
-            list = Voting.objects.filter(start_date__isnull=True, end_date__isnull=True).all()
-        else:
-            if busqueda is None:
-                list = Voting.objects.all()
-            else:
-                list = Voting.objects.filter(name__contains=busqueda).all()
+        votings = Voting.objects.all()
+        abstm = 0
+        porvotm = 0
+        vtm = 0
+        ctm = 0
+        abstr = 999999999999999999
+        porvotr = 999999999999999999
+        vtr = 999999999999999999
+        ctr = 999999999999999999
+        tabst = 0
+        tporvot = 0
+        for vot in votings:
+            if vot.end_date:
+                vid = vot.id
+                census = Census.objects.filter(voting_id=vid).all().count()
+                votes = Vote.objects.filter(voting_id=vid).all().count()
+                #Votos totales y mayor / menor
+                vt = vt + votes
+                if vtm < votes:
+                    vtm = votes
+                if vtr > votes:
+                    vtr = votes
+                #Censo total y mayor / menor
+                ct = ct + census
+                if ctm < census:
+                    ctm = census 
+                if ctr > census:
+                    ctr = census
+
+                #Numero de votaciones
+                nvs = nvs + 1
+                #Porcentaje de abstencion y mayor / menor
+                if census != 0:
+                    abst = ((votes/census)-1)*(-100)
+                    tabst = tabst + abst
+                    if abstm < abst:
+                        abstm = abst
+                    if abstr > abst:
+                        abstr = abst
+                #Porcentaje de voto y mayor / menor
+                    porvot = (votes/census)*100
+                    tporvot = tporvot + porvot
+                    if porvotm < porvot:
+                        porvotm = porvot
+                    if porvotr > porvot:
+                        porvotr = porvot
+                        
     except:
+            
         raise Http404
-    #Si no soy superuser solo veo las votaciones en las que estoy censado
-    if not request.user.is_superuser:
-        census = Census.objects.filter(voter_id=request.user.id).all()
-        new_list = []
-        for c in census:
-            for voting in list:
-                if voting.id == c.voting_id:
-                    new_list.append(voting)
-        list = new_list
-        user = False
-    else:
-        user = True
-    return render(request, 'visualizer/listVisualizer.html', {'votings': list, 'user': user})
+        
+    return render(request, 'visualizer/globalVisualizer.html', {'votes': vt, 'votesm': vtm, 'census': ct, 'censusm': ctm, 'nvoting': nvs, 
+    'abst': tabst, 'abstm': abstm, 'porvot': tporvot, 'porvotm': porvotm, 'porvotr': porvotr, 'abstr': abstr, 'votesr': vtr, 'censusr': ctr})
