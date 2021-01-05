07ed82a581fe7b99d605859e4d1af598d01dedbd
DiegoHorrilloDuran <petakaaracena@gmail.com>
2021-01-06 00:57:15 +0100

    003 - Añadir funciones de visualizacion
    Añadida función para graficas del tipo identity con un bug que hace desaparecer la grafica al cargarla

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -17,37 +17,32 @@ class VisualizerView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
-        labels = []
-        data = []
 
         try:
             r = mods.get('voting', params={'id': vid})
             context['voting'] = json.dumps(r[0])
-            context['postproc'] = r[0]["postproc"][0]
-            print(context)
         except:
             raise Http404
 
-        #postproc = r[0].get('postproc')
-        postproc = [
-            {'option': 'Option E', 'number': 5, 'points': 21, 'votes': 391000, 'postproc': 8},
-            {'option': 'Option F', 'number': 6, 'points': 21, 'votes': 311000, 'postproc': 7},
-            {'option': 'Option B', 'number': 2, 'points': 21, 'votes': 184000, 'postproc': 4},
-            {'option': 'Option C', 'number': 3, 'points': 21, 'votes': 73000, 'postproc': 2},
-            {'option': 'Option G', 'number': 7, 'points': 21, 'votes': 27000, 'postproc': 0},
-            {'option': 'Option A', 'number': 1, 'points': 21, 'votes': 12000, 'postproc': 0},
-            {'option': 'Option D', 'number': 4, 'points': 21, 'votes': 2000, 'postproc': 0},
-        ]
-
-        for voting in postproc :
-            labels.append(voting['option'])
-            data.append(int(voting['postproc']))
-
-        context['labels'] = labels
-        context['data'] = data
+        try:
+            labels, data = self.get_identity_data(r[0])
+            context['labels'] = labels
+            context['data'] = data
+        except:
+            return context
 
         return context
 
+    def get_identity_data(self,voting):
+        labels = []
+        data = []
+        postproc = voting.get('postproc')
+
+        for voting in postproc[0]:
+            labels.append(voting['option'])
+            data.append(int(voting['postproc']))
+
+        return labels,data
 
 def get_list_votings(request):
     filter = request.GET.get('filter')
