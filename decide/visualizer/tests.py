a58d0f119fbed86410e8fe68c016116521cf3956
Carlos <74050501+makislayer@users.noreply.github.com>
2021-01-06 23:31:31 +0100

    020 - Tests vista estadisticas Creados 4 test básicos de visualización

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
-
+from base.tests import BaseTestCase
 # Create your tests here.
+
+class Statistics_View_Tests(BaseTestCase):
+    fixtures = ['visualizer/migrations/populate.json', ]
+
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_get_detail_voting_20(self):
+        response = self.client.get('/visualizer/20/statistics')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_detail_voting_21(self):
+        response = self.client.get('/visualizer/21/statistics')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_detail_voting_22(self):
+        response = self.client.get('/visualizer/22/statistics')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_detail_voting_404(self):
+        response = self.client.get('/visualizer/10/statistics')
+        self.assertEqual(response.status_code, 404)
