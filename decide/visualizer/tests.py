d64767b9f5ba5c3df6190c158f453bffa49019ff
Adrrolroj <adrrolroj@alum.us.es>
2021-01-06 22:25:54 +0100

    019 - Test listado de visualizacion de votaciones
    Añadidos 2 test para el metodo de mostrar el listado de votaciones

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,25 @@
+from authentication.models import UserProfile
 from django.test import TestCase
 
 # Create your tests here.
+from voting.models import Voting
+
+from base.tests import BaseTestCase
+
+
+class List_View_Tests(BaseTestCase):
+    fixtures = ['visualizer/migrations/populate.json', ]
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_get_list_voting_200(self):
+        response = self.client.get('/visualizer/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_votings_from_list_voting_anonymous(self):
+        response = self.client.get('/visualizer/')
+        votings = response.context['votings']
+        self.assertEqual(votings, [])
