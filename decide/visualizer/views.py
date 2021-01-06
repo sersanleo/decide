ea40d9de3186ce97b267e333c9a863839efd8279
MCLLC96 <marllacar1@alum.us.es>
2021-01-07 00:40:18 +0100

    003-Añadir funciones de visualizacion Añadido el grafico para el tipo 'equality'

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -7,7 +7,6 @@ from django.http import Http404
 from base import mods
 
 from voting.models import Voting
-
 from census.models import Census
 
 
@@ -19,15 +18,47 @@ class VisualizerView(TemplateView):
         vid = kwargs.get('voting_id', 0)
 
         try:
+            voting = Voting.objects.get(pk = vid)
             r = mods.get('voting', params={'id': vid})
             context['voting'] = json.dumps(r[0])
-            context['postproc'] = r[0]["postproc"][0]
-            print(context)
+
+            # Mostramos las gráficas de las votaciones finalizadas
+            if not voting.end_date == None:
+
+                #if voting.type == 'EQUALITY':
+                    self.statistics_equality(context, voting)
+                #elif voting.type == 'IDENTITY':
+
+                #else:
+
         except:
             raise Http404
 
         return context
 
+    def statistics_equality(self, context, voting):
+        
+        postproc = [
+            { 'option': 'Option 1', 'number': 1, 'votes_men': 2, 'votes_women': 3, 'postproc': 4 },
+            { 'option': 'Option 3', 'number': 3, 'votes_men': 3, 'votes_women': 1, 'postproc': 4 },
+            { 'option': 'Option 2', 'number': 2, 'votes_men': 0, 'votes_women': 4, 'postproc': 3 },
+            { 'option': 'Option 5', 'number': 5, 'votes_men': 1, 'votes_women': 3, 'postproc': 3 },
+            { 'option': 'Option 6', 'number': 6, 'votes_men': 1, 'votes_women': 1, 'postproc': 2 },
+            { 'option': 'Option 4', 'number': 4, 'votes_men': 1, 'votes_women': 0, 'postproc': 1 }]
+
+        options = []
+        v_men = []
+        v_women = []
+        
+        for opt in postproc:
+            options.append(opt['option'])
+            v_men.append(opt['votes_men'])
+            v_women.append(opt['votes_women'])
+        
+        context['options'] = options
+        context['v_men'] = v_men
+        context['v_women'] = v_women
+        
 
 def get_list_votings(request):
     filter = request.GET.get('filter')
@@ -60,4 +91,5 @@ def get_list_votings(request):
     else:
         user = True
     return render(request, 'visualizer/listVisualizer.html', {'votings': list, 'user': user})
+
 
