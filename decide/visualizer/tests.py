f5c5a2429ede3453bec4525b784727b76e58200b
Carlos <74050501+makislayer@users.noreply.github.com>
2021-01-06 23:31:31 +0100

    020 - Tests vista estadisticas Creados 4 test básicos de visualización

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -21,7 +21,7 @@ class AdminTestCase(StaticLiveServerTestCase):
 		options = webdriver.ChromeOptions()
 		options.headless = True
 		self.driver = webdriver.Chrome(options=options)
-	
+
 		super().setUp()
 
 	def tearDown(self):
@@ -92,19 +92,46 @@ class AdminTestCase(StaticLiveServerTestCase):
 
 
 class List_View_Tests(BaseTestCase):
-    fixtures = ['visualizer/migrations/populate.json', ]
-    def setUp(self):
-        super().setUp()
+	fixtures = ['visualizer/migrations/populate.json', ]
+	def setUp(self):
+		super().setUp()
 
-    def tearDown(self):
-        super().tearDown()
 
-    def test_get_list_voting_200(self):
-        response = self.client.get('/visualizer/')
-        self.assertEqual(response.status_code, 200)
+	def tearDown(self):
+		super().tearDown()
 
-    def test_get_votings_from_list_voting_anonymous(self):
-        response = self.client.get('/visualizer/')
-        votings = response.context['votings']
-        self.assertEqual(votings, [])
 
+	def test_get_list_voting_200(self):
+		response = self.client.get('/visualizer/')
+		self.assertEqual(response.status_code, 200)
+
+
+	def test_get_votings_from_list_voting_anonymous(self):
+		response = self.client.get('/visualizer/')
+		votings = response.context['votings']
+		self.assertEqual(votings, [])
+
+class Statistics_View_Tests(BaseTestCase):
+	fixtures = ['visualizer/migrations/populate.json', ]
+
+	def setUp(self):
+		super().setUp()
+
+	def tearDown(self):
+		super().tearDown()
+
+	def test_get_detail_voting_20(self):
+		response = self.client.get('/visualizer/20/statistics')
+		self.assertEqual(response.status_code, 200)
+
+	def test_get_detail_voting_21(self):
+		response = self.client.get('/visualizer/21/statistics')
+		self.assertEqual(response.status_code, 200)
+
+	def test_get_detail_voting_22(self):
+		response = self.client.get('/visualizer/22/statistics')
+		self.assertEqual(response.status_code, 200)
+
+	def test_get_detail_voting_404(self):
+		response = self.client.get('/visualizer/10/statistics')
+		self.assertEqual(response.status_code, 404)
