5a16e318658ef88c6ab01b9c6acb8b0c6cad09b6
danimilles <dansanbal@alum.us.es>
2021-01-15 22:11:17 +0100

    postprocesado - #463 - Cambios en travis.yml para despliegue
    postprocesado - #463 - Cambios en travis.yml para despliegue

--- decide/travis_local_settings.py
+++ decide/travis_local_settings.py
@@ -30,7 +30,7 @@ BASEURL = 'http://localhost:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_decide',
+        'NAME': 'decide',
         'USER': 'decide',
         'PASSWORD': 'decide',
         'HOST': 'localhost',
