58fd79ce359dc9cce9ac2d7fc065ba69fc8ce8e2
josvilgar1 <josvilgar1@alum.us.es>
2021-01-16 11:51:37 +0100

    006 - Mejora calidad del codigo y correccion de errores
    Hemos mejorado la visualizacion de la lista de votaciones a visualizar.
    Arreglada la visualizacion de las funciones con atributo Points.

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from .views import VisualizerViewPointsInclude
 from .views import StatisticsView
 from .views import get_list_votings
 from .views import VisualizerView, get_global_view
@@ -9,6 +8,5 @@ urlpatterns = [
     path('global', get_global_view),
     path('<int:voting_id>/statistics', StatisticsView.as_view()),
     path('', get_list_votings),
-    path('<int:voting_id>/functions', VisualizerViewPointsInclude.as_view())
 ]
 
