e05233fe64e6bde42d955e3bdc2001bd752d8ef3
raugarde <raulgarciadelapoza@gmail.com>
2021-01-16 10:53:24 +0000

    postprocesado-#289 - Test negativos recuento Hondt

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -439,7 +439,36 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
-    
+
+    def test_hondt_without_votes(self):
+        data = [{
+            'type': 'HONDT',
+            'options': [
+                {'option': 'Option 1', 'number': 1, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0},
+                {'option': 'Option 2', 'number': 2, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0},
+                {'option': 'Option 3', 'number': 3, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0},
+                {'option': 'Option 4', 'number': 4, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0},
+                {'option': 'Option 5', 'number': 5, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0},
+            ]
+        }]
+
+        expected_result = [{
+            'type': 'HONDT',
+            'options': [
+                {'option': 'Option 1', 'number': 1, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0, 'postproc': 0},
+                {'option': 'Option 2', 'number': 2, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0, 'postproc': 0},
+                {'option': 'Option 3', 'number': 3, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0, 'postproc': 0},
+                {'option': 'Option 4', 'number': 4, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0, 'postproc': 0},
+                {'option': 'Option 5', 'number': 5, 'points': 7, 'votes_masc': 0, 'votes_fem': 0, 'votes': 0, 'postproc': 0},
+            ]
+        }]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
 
     def test_droop(self):
         data = [{
