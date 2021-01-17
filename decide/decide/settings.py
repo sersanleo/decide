cfdce8c2512a87b847847efe98990479a303427f
Sergio Sánchez León <56033905+sersanleo@users.noreply.github.com>
2021-01-17 20:50:43 +0100

    usabilidad-389
    Debug = False (Heroku)

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
