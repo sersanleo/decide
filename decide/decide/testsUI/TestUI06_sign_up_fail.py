2df4eac497090f9a5889d87d6bf4ea986551426e
albparmor2 <albparmor2@alum.us.es>
2021-01-17 00:46:43 +0000

    usabilidad-h400
    Añadiendo un import a uno de los tests

--- decide/decide/testsUI/TestUI06_sign_up_fail.py
+++ decide/decide/testsUI/TestUI06_sign_up_fail.py
@@ -10,6 +10,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+from base.tests import BaseTestCase
+
 class TestFail():
   def setup_method(self, method):
     self.base = BaseTestCase()
