17aee195fadf715d1f0adde182257b406cf38c30
sersanleo <sersanleo@alum.us.es>
2021-01-19 20:42:00 +0100

    usabilidad-1
    Añadido el idioma francés y cookie para recordar idioma.

--- decide/base/static/js/script.js
+++ decide/base/static/js/script.js
@@ -3,10 +3,19 @@
 $.getJSON('static/js/lang.json', function(json){
   $('.translate').click(function(){
     let lang = $(this).attr('id');
+    document.cookie = 'lang='+lang+';path=/;';
     $('.lang').each(function(index, element){
       element.innerText=json[lang][element.id];
      // $(this).text(json[lang][$(this).attr('id')]);
     });
+  });
+
+  var cookies = document.cookie.split("; ");
+  cookies.forEach((c) => {
+      var cs = c.split("=");
+      if (cs[0] == 'lang' && cs[1]) {
+        $('#'+cs[1]).click();
+      }
   });
 
 });//Get json AJAX
